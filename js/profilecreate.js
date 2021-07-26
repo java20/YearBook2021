8db72f4d430b07481ac4d50fbf773b50726ea591
java20 <harshshah7060@gmail.com>
2021-07-26 15:29:05 +0530

    Added all student profiles

---
+++ js/profilecreate.js
@@ -0,0 +1,329 @@
+
+const student_responses = [
+  {
+    "Full Name": "Aishwarya sunil kardile",
+    "Small Message or Quotes": "College life is that part of your life that you are going to relive in your memories till you breathe",
+    "Add Image for your Profile Photo": "img\\profileimages\\AISHWARYAProfile.JPG",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Azhar Hakim",
+    "Small Message or Quotes": "Never gone a forget those mesmerizing Days of the College.",
+    "Add Image for your Profile Photo": "img\\profileimages\\AzharProfile.jpg",
+    "Any message or memories to share": "Kitne jldi ye din chale gaye..\nHaste gaate ye khubsurat lamhe bit gaye...\nNa kabhi socha tha ki wqt itne jldi badalta hai ..\nAisa lagta hai ki kal hi ki baat haii..\nJab hum aye the ye college mei..\nMujhe yaad hai woh wqt subah ka tha .\nTazi hawayee chal rahe the khuhnuma mosam tha.. \nWo pagal se jaan lagane wale yaar the ..\nWo daat , pyaar ke sath dil se padane wale staff the..\nHar wqt mauj mashti ke sath chalte chalte...\nYe akhri padav aa gaya hai  ab..\nYe college ki yaadeii jo dil ke bakse  me band ho chuke hai ab..\nNa kabhi bhulenge ya bhul payenge  ye Hansen Pal ..\nAisa lagta hai ki kal hi ki baat hai jab hum sab sath chalte the college me apne yaaro ke sang...\nAzhar Hakim",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=19Q2RsJxqNGv2Sbij1o8nE_X3_cGQxDQ6",
+    "Any Description of event": "Celebration of TEACHER'S Day .",
+    "Any photos or video of event": "https://drive.google.com/open?id=1E3GwTimXy-ewRUoJE8dIouAovi2FgY-S",
+    "Title for Memories": "Kal hi ki baat haii ..",
+    "Title for Event": "Teacher Day Event",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Bibek Sarkar",
+    "Small Message or Quotes": "No",
+    "Add Image for your Profile Photo": "img\\profileimages\\BibekProfile.jpg",
+    "Any message or memories to share": "No",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "No",
+    "Any photos or video of event": "",
+    "Title for Memories": "No",
+    "Title for Event": "no",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Biraj Sarkar",
+    "Small Message or Quotes": "We say it best when we say nothing at all",
+    "Add Image for your Profile Photo": "img\\profileimages\\BirajProfile.jpg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Ekta Rajpal Yadav",
+    "Small Message or Quotes": "Do not follow where the path may lead. Go, instead, where there is no path and leave a trail",
+    "Add Image for your Profile Photo": "img\\profileimages\\EktaProfile.jpg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Hamza Sayyad",
+    "Small Message or Quotes": "Accept failures as a part of process!",
+    "Add Image for your Profile Photo": "img\\profileimages\\HamzaProfile.jpg",
+    "Any message or memories to share": "Memories that wont fade away easily.?",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1RLWRRQvms09DlaaeAlqp5ecO3wavojTN",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Cultural Event 2k20",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Insharah Shaikh",
+    "Small Message or Quotes": "I hate having to explain to everyone why I wear a hijab but if everyone must know: Voldemort has possessed me and his face is living on the back of my head.",
+    "Add Image for your Profile Photo": "img\\profileimages\\InsharahProfile.jpeg",
+    "Any message or memories to share": "Loved  that our college allowed traditional  day as it helps us to understand each other's traditions.",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1dJjGZS0nWUxoOAGfCzq1uVpestI1NLAk",
+    "Any Description of event": "",
+    "Any photos or video of event": "https://drive.google.com/open?id=1NrI7QEbKzvJzOnHOLSZltlG6-6H4TgDq",
+    "Title for Memories": "Traditional day",
+    "Title for Event": "Saree day",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Juveriya Yunus Nadaf",
+    "Small Message or Quotes": ".",
+    "Add Image for your Profile Photo": "img\\profileimages\\JuveriyaProfile.JPG",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1Qm-hLYjdtALW43A2CrgsB7sS7c08chOD",
+    "Any Description of event": ".",
+    "Any photos or video of event": "https://drive.google.com/open?id=15nXOKV3idTsiJRUmZaJqSfg7Ut1CqBil",
+    "Title for Memories": "Miss KJEI's",
+    "Title for Event": "Youth fest",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Mandar Mahesh Pimparkar",
+    "Small Message or Quotes": "College gave some of the best moments in life.",
+    "Add Image for your Profile Photo": "img\\profileimages\\MandarImage.jpg",
+    "Any message or memories to share": ".",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": ".",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "More Dayanand Sampat",
+    "Small Message or Quotes": "Things ends, but memories last forever.",
+    "Add Image for your Profile Photo": "img\\profileimages\\DayanandProfile.jpeg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Neha Anil Rawade",
+    "Small Message or Quotes": "-",
+    "Add Image for your Profile Photo": "img\\profileimages\\NehaProfile.jpg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1Xc2Nomltjodp3kbHVwzxTqGCLPC5QVw0",
+    "Any Description of event": "-",
+    "Any photos or video of event": "",
+    "Title for Memories": "Shivajayanti",
+    "Title for Event": "-",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Pooja Dattatray",
+    "Small Message or Quotes": "We didn't realise we were making memories we just knew we were having fun.College's cultural fest is one of the best experiences in college life.",
+    "Add Image for your Profile Photo": "img\\profileimages\\PoojaProfile.jpg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1Yz_qizWIcr2BaGjPqECRWDpC2LLlkMlH",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Cultural activities and Days",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "priya jadhav",
+    "Small Message or Quotes": "the college has given me best memories.",
+    "Add Image for your Profile Photo": "img\\profileimages\\PriyaProfile.jpg",
+    "Any message or memories to share": ".",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=10ZofPJMo2ZzJXcN0fBedo50YTApkd-Yq",
+    "Any Description of event": "",
+    "Any photos or video of event": "https://drive.google.com/open?id=1Czn99LH2dt_yZ2KU6E3S-S3l6zuhZLpk",
+    "Title for Memories": "yaadein",
+    "Title for Event": "kabaddi",
+    "Email Address": "priyajadhav51@gmail.com"
+  },
+  {
+    "Full Name": "Priyanka Hallale",
+    "Small Message or Quotes": "�Live for the moments you can�t put into words� and yes today I am running out of words to write here :) My college life was a beautiful journey ! Every faculty here were so supporting, I thank our Principal Sir who always encouraged, guided and gave us needy facilities. I will surely miss my friends who are so helpful. I would say \"Thanks a million TAE for everything\".",
+    "Add Image for your Profile Photo": "img\\profileimages\\PriyankaProfile.jpg",
+    "Any message or memories to share": "KJ college fest had 3-4 anchoring auditions, on my 1st day of audition I was very nervous seeing so many students from all 4 KJ institute who came for audition, they were so strong and great in speaking ! The movement I had to stand on stage and speak for audition I was numb, I couldn't speak a single word ! I said sorry and was walking out of the room suddenly the judge stopped me and said \"Its ok, this happens for new comers\" He motivated me to speak up and yes I was selected in audition and meanwhile won Gold Medal in anchoring, it was great opportunity for me to stand alone in front of such a huge crowd and to make audience enjoy my anchoring with applause was challenging experience !!! After few days of winning this Gold medal I was travelling with my mother in crowded local bus, I was away from my mom and my face was covered with stole still two unknown girls from KJ college who were able to recognize me as KJ fest anchor and my mother was sitting next to them, they appreciated my anchoring, and they were repeating my dialogue line by line :) My mom was so happy that day, this was my first recognition for anchoring, later this kept on happening with me, many people appreciated my work. This was possible only because of that 2nd opportunity for anchoring audition, I can't put in words my thankfulness to this college who gave me opportunities, platform and name. Thanks a million !",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1sN5rA8BUakDLVvmmZ44e0N-iD3v8BWic",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "KJ Fest Malhar Anchoring Event",
+    "Title for Event": "",
+    "Email Address": "hallalepihu@gmail.com"
+  },
+  {
+    "Full Name": "Rohan Bhalerao",
+    "Small Message or Quotes": "An investment in knowledge always pays the best interest",
+    "Add Image for your Profile Photo": "img\\profileimages\\RohanProfile.jpeg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Grate experience with friends, teachers, and college surroundings.",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Rohit Saggam",
+    "Small Message or Quotes": "YOLO Make it worth",
+    "Add Image for your Profile Photo": "img\\profileimages\\RohitProfile.jpg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1vEQZzxOvEhMNhxqncAMw5vEsGfpGEBHl",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Roshani Gokul Kale",
+    "Small Message or Quotes": "college was a best time of my life, Things end but memories last forever.",
+    "Add Image for your Profile Photo": "img\\profileimages\\RoshaniProfile.jpg",
+    "Any message or memories to share": "Graduation is an exciting time . Its both ending and a begining. its a warm memories of the past and big dreams for the future.\nIn TAE we enjoyed every moment with friends & with teachers. here we learned how to deal with situations, learned new things with enjoyments. TAE gave me strength, confidence and most important precious friends and cool teachers thank you TAE !!!",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1UA0S58ysnPLd_88wXhufAFq78sScdydS",
+    "Any Description of event": "Kjei cultural events was a very exciting and most awaited event in our life. where we enjoyed every little moments , bunks classes and practices a lot. I took part in cultural activities from first year to third year where i got stage confidence and strength. Every teachers , TAE faculty , principle are very supportive. I spend my 4 years here with good environment and positive atmosphere.",
+    "Any photos or video of event": "https://drive.google.com/open?id=1Vm4JparH3q6_N-VrfnFuzaLGV8AMPV-e",
+    "Title for Memories": "Precious Memories with Friends and Teachers",
+    "Title for Event": "TAE Cultural Memories",
+    "Email Address": "kaleroshani905@gmail.com"
+  },
+  {
+    "Full Name": "Satish Belwadkar",
+    "Small Message or Quotes": "A big thank you to my friends, family, and teachers for making me who I am today.",
+    "Add Image for your Profile Photo": "img\\profileimages\\SatishProfile.png",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1mapjaDzknWwDTpHwFosDwjkk81_I1a8e",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Memories",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Shahid Khan",
+    "Small Message or Quotes": "Though we were robbed of our precious 1� year here, the time spent here was really the days of our lives!!",
+    "Add Image for your Profile Photo": "img\\profileimages\\ShahidProfile.jpg",
+    "Any message or memories to share": "During our FE, we got one off!! And guess what were we doing!! Playing mini militia on the same server shouting, yelling even hitting our teammates.",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1xXYCCpGNL4TADEiDupOybFbMiOXtAbsW",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "The off-lecture",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Shipra Banerjee",
+    "Small Message or Quotes": "Its been an amazing journey throughout. Amazing bcoz of my friends and each one of them i met . The memories we captured will be there with me lifetime which always brings a priceless smile on my face whenever refreshred. The baseless jokes, making our friend stand ahead whenever its time for punishments. That masti that enjoyment that pagalpan will be missed. #2peas#bunkers",
+    "Add Image for your Profile Photo": "img\\profileimages\\ShipraProfile.jpg",
+    "Any message or memories to share": "Starting from the first lec every teacher enters and we were like happy birthday mandar.. We were more excited than our bday boy and t last the day emds up with teacher scolding and punishments.",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1ilrhMKuMbe_EewU51Z9KKP_fN6Gk7_7t",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Mandar's birthday",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Sneha Sambhaji Patil",
+    "Small Message or Quotes": "In last four years I have learnt lots of things from my friends and teachers .I had the best time of my life in college, and my college life memories will always make me happy. Thank you all of you for such great memories . Miss you all !!!!",
+    "Add Image for your Profile Photo": "img\\profileimages\\SnehaProfile.jpeg",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1wsnOK1TUKiDwP8eDQWqJti0ccQq1_bw9",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Sohel Shaikh",
+    "Small Message or Quotes": ".......",
+    "Add Image for your Profile Photo": "img\\profileimages\\SohelProfile.png",
+    "Any message or memories to share": "Nature , Friends , Bonds and Memories",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1gh2w7_Wy32vCogM2pu2IUKrczLTO1Suu",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "Trip",
+    "Title for Event": "",
+    "Email Address": ""
+  },
+  {
+    "Full Name": "Tejaswini Prabhakar Nikam",
+    "Small Message or Quotes": "To achieve anything and move forward in life, you need to take risks.",
+    "Add Image for your Profile Photo": "img\\profileimages\\TejaswiniProfile.jpg",
+    "Any message or memories to share": "Things end but memories last forever??????",
+    "Any photos or video with college, Faculty, Friends": "https://drive.google.com/open?id=1ZIYG15X7oGj1TDU0OS0lP3clg5XnlEw6",
+    "Any Description of event": "",
+    "Any photos or video of event": "https://drive.google.com/open?id=1Of4sjojVx2RvTrIi4yoyuQzA0671r8xy",
+    "Title for Memories": "",
+    "Title for Event": "Youth fest",
+    "Email Address": "ranipnikam@gmail.com"
+  },
+  {
+    "Full Name": "Tushar kashinath Gaikawad",
+    "Small Message or Quotes": "Journey",
+    "Add Image for your Profile Photo": "img\\profileimages\\TusharProfile.JPG",
+    "Any message or memories to share": "",
+    "Any photos or video with college, Faculty, Friends": "",
+    "Any Description of event": "",
+    "Any photos or video of event": "",
+    "Title for Memories": "",
+    "Title for Event": "",
+    "Email Address": ""
+  }
+];
+
+function loadProfiles() {
+  let ele = document.getElementById('profilePages');
+  let markup;
+  for (let i = 0; i < student_responses.length; i++) {
+    if(i%4==0){
+      markup = `
+      <div class="my-page page user_profile">
+					<div class="user_profile__container">
+						<figure class="user_profile__card">
+							<img src="${student_responses[i]["Add Image for your Profile Photo"]}" alt="${student_responses[i]["Full Name"]}"
+								class="user_profile__image" />
+							<figcaption class="user_profile__name">${student_responses[i]["Full Name"]}</figcaption>
+						</figure>
+						<figure class="user_profile__card">
+							<img src="${student_responses[i+1]["Add Image for your Profile Photo"]}" alt="${student_responses[i+1]["Full Name"]}"
+								class="user_profile__image" />
+							<figcaption class="user_profile__name">${student_responses[i+1]["Full Name"]}</figcaption>
+						</figure>
+						<figure class="user_profile__card">
+							<img src="${student_responses[i+2]["Add Image for your Profile Photo"]}" alt="${student_responses[i+2]["Full Name"]}"
+								class="user_profile__image" />
+							<figcaption class="user_profile__name">${student_responses[i+2]["Full Name"]}</figcaption>
+						</figure>
+						<figure class="user_profile__card">
+							<img src="${student_responses[i+3]["Add Image for your Profile Photo"]}" alt="${student_responses[i+3]["Full Name"]}"
+								class="user_profile__image" />
+							<figcaption class="user_profile__name">${student_responses[i+3]["Full Name"]}</figcaption>
+						</figure>
+					</div>
+				</div>
+  `
+      ele.insertAdjacentHTML("beforeend",markup)
+    }
+  }
+}
+
+loadProfiles();
